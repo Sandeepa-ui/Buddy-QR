0f25ff766992f3512c8761ff4f4f59f8e567a0ee
Sandeepa-ui <sandeepah20020916@gmail.com>
2026-09-14 15:18:20 +0530

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -6,5 +6,5 @@
  */
 window.BSC_CLOUD_CONFIG = {
   url: 'https://script.google.com/macros/s/AKfycbzKoyDGS4nuEe8FHIaLtn7oq-s-eyQDzipAReH-Girf8MpZ6AwW5jQtnBr6-6_4AUun/exec',
-  token: ''
+  token: '1dc6387dc4c3425780a7f325b412b367'
 };
