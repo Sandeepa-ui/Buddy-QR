1b9b767f346525b291284c4fa7da88e0c2f613b4
Sandeepa <sandeepa20020916@gmail.com>
2026-09-14 14:17:18 +0530

    Update Apps Script Web App URL

--- cloud-config.js
+++ cloud-config.js
@@ -5,6 +5,6 @@
  * in Settings inside the app; do not commit a real token to a public repo.
  */
 window.BSC_CLOUD_CONFIG = {
-  url: '',
+  url: 'https://script.google.com/macros/s/AKfycbzKoyDGS4nuEe8FHIaLtn7oq-s-eyQDzipAReH-Girf8MpZ6AwW5jQtnBr6-6_4AUun/exec',
   token: ''
 };
